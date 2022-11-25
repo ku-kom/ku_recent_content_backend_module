5cd4a31921132b5dd85070209ec5963450e7d069
nanna-dk <nanna@e-nanna.dk>
2022-11-25 15:34:21 +0100

    Remove sql

--- ext_tables.sql
+++ ext_tables.sql
@@ -2,7 +2,3 @@
 # Add SQL definition of database tables
 #
 # KU backend module
-CREATE TABLE pages (
-  responsible_name varchar(255) DEFAULT '' NOT NULL,
-  responsible_email varchar(255) DEFAULT '' NOT NULL,
-);
