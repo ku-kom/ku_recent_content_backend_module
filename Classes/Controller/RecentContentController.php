5d461834770c47ed8ef1be46aa57f31f6771664c
nanna-dk <nanna@e-nanna.dk>
2022-12-05 10:34:15 +0100

    Remove dependency

--- Classes/Controller/RecentContentController.php
+++ Classes/Controller/RecentContentController.php
@@ -13,7 +13,6 @@ use TYPO3\CMS\Core\Domain\Repository\PageRepository;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Core\Page\AssetCollector;
 use TYPO3\CMS\Core\Database\ConnectionPool;
-use TYPO3\CMS\Core\Localization\LanguageService;
 use TYPO3\CMS\Core\Imaging\IconFactory;
 use TYPO3\CMS\Core\Imaging\Icon;
 use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
